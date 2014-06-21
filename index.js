4a0b6fdd0e03dfe99ffba76aa419a89994336537
Zeph Lafassett <zeph@lafassett.com>
2014-06-21 09:53:20 -0700

    Adding upload tests

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var express = require('express');
+var express = require('express'),
+	fs = require('fs'),
+	path = require('path'),
+	multiparty = require('multiparty');
 
 var app = express();
 
@@ -7,6 +10,36 @@ app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
 	res.render('index', {});
+});
+
+app.get('/upload', function (req, res) {
+	res.render('upload', {});
+});
+
+
+app.post('/upload', function (req, res) {
+	var form = new multiparty.Form();
+	var dest = path.join(__dirname, 'files');
+
+	form.on('error', function (err) {
+		throw err;
+	});
+
+	form.on('close', function () {
+		res.send('file uploaded');
+	});
+
+	form.on("file", function (name, file) {
+		var new_file = path.join(dest, file.originalFilename);
+		fs.rename(file.path, new_file, function (err) {
+			if (err) {
+				throw err;
+			}
+			console.log('file has been moved: ' + file.path + ' -> ' + new_file);
+		});
+	});
+
+	form.parse(req);
 });
 
 app.listen(3000);
