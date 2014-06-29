5233f198c195b8211f23d324f70ed809c84945c8
Zeph Lafassett <zeph@lafassett.com>
2014-06-29 10:35:04 -0700

    Adding more exif stuff

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var express = require('express'),
 	config = require('./config/config.json'),
 	path = require('path'),
 	serveStatic = require('serve-static'),
+	exif = require('exif'),
 	multiparty = require('multiparty');
 
 var app = express();
@@ -42,6 +43,23 @@ app.post('/upload', function (req, res) {
 				throw err;
 			}
 			console.log('file has been moved: ' + file.path + ' -> ' + new_file);
+			new exif.ExifImage({
+				image: new_file
+			}, function (err, data) {
+				if (err) {
+					throw err;
+				}
+				console.dir(data);
+				if (data.gps) {
+					var lat = convert(data.gps.GPSLatitude[0], data.gps.GPSLatitude[1], data.gps.GPSLatitude[2]);
+					var lon = convert(data.gps.GPSLongitude[0], data.gps.GPSLongitude[1], data.gps.GPSLongitude[2]);
+					if (data.gps.GPSLongitudeRef === 'W') {
+						lon = lon * -1;
+					}
+					console.log('GPSLatitude', lat);
+					console.log('GPSLongitude', lon);
+				}
+			});
 		});
 	});
 
@@ -49,3 +67,16 @@ app.post('/upload', function (req, res) {
 });
 
 app.listen(3000);
+
+
+function convert(deg, min, sec) {
+	var azi;
+	if (deg < 0) {
+		azi = -1.0 * deg + 1.0 * min / 60.0 + 1.0 * sec / 3600.0;
+		return -1.0 * azi;
+	} else {
+		azi = 1.0 * deg + 1.0 * min / 60.0 + 1.0 * sec / 3600.0;
+		return azi;
+	}
+}
+
