24aba2365ed2dd834e03a48e3b949212afb14dc1
Zeph Lafassett <zeph@lafassett.com>
2014-07-06 09:14:55 -0700

    Adding upload directory check on startup

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ var express = require('express'),
 	exif = require('exif'),
 	multiparty = require('multiparty');
 
+// check to see if the uploads directory exists.
+var upload_path = path.join(__dirname, config.uploadPath);
+if (!fs.existsSync(upload_path)) {
+	try {
+		fs.mkdirSync(upload_path);
+	} catch (e) {
+		throw e;
+	}
+} else {
+	if (!fs.lstatSync(upload_path).isDirectory()) {
+		throw new Error("the upload path is not a directory!");
+	}
+}
+
+// start up the application
 var app = express();
 
 app.set('views', __dirname + '/views');
@@ -18,75 +33,6 @@ app.use('/', router);
 
 app.use(serveStatic(path.join(__dirname, '/views/static')));
 
-// app.get('/', function (req, res) {
-// 	res.render('index', {});
-// });
-
-// app.get('/upload', function (req, res) {
-// 	res.render('upload', {});
-// });
-
-// app.get('/map', function (req, res) {
-// 	res.render('map', {apiKey: config.google.maps.key});
-// });
-
-// app.post('/upload', function (req, res) {
-// 	var form = new multiparty.Form();
-// 	var dest = path.join(__dirname, 'files');
-
-// 	form.on('error', function (err) {
-// 		throw err;
-// 	});
-
-// 	form.on('close', function () {
-// 		res.send('file uploaded');
-// 	});
-
-// 	form.on("file", function (name, file) {
-// 		var new_file = path.join(dest, file.originalFilename);
-// 		fs.rename(file.path, new_file, function (err) {
-// 			if (err) {
-// 				throw err;
-// 			}
-// 			console.log('file has been moved: ' + file.path + ' -> ' + new_file);
-// 			new exif.ExifImage({
-// 				image: new_file
-// 			}, function (err, data) {
-// 				if (err) {
-// 					throw err;
-// 				}
-// 				console.dir(data);
-// 				if (data.gps) {
-// 					var lat = convert(data.gps.GPSLatitude[0], data.gps.GPSLatitude[1], data.gps.GPSLatitude[2]);
-// 					var lon = convert(data.gps.GPSLongitude[0], data.gps.GPSLongitude[1], data.gps.GPSLongitude[2]);
-// 					if (data.gps.GPSLongitudeRef === 'W') {
-// 						lon = lon * -1;
-// 					}
-// 					console.log('GPSLatitude', lat);
-// 					console.log('GPSLongitude', lon);
-// 				} else {
-// 					console.log("couldn't locate GPS data from image");
-// 				}
-// 			});
-// 		});
-// 	});
-
-// 	form.parse(req);
-// });
-
 app.listen(3000, function () {
 	console.log("listening on port: 3000");
 });
-
-
-function convert(deg, min, sec) {
-	var azi;
-	if (deg < 0) {
-		azi = -1.0 * deg + 1.0 * min / 60.0 + 1.0 * sec / 3600.0;
-		return -1.0 * azi;
-	} else {
-		azi = 1.0 * deg + 1.0 * min / 60.0 + 1.0 * sec / 3600.0;
-		return azi;
-	}
-}
-
