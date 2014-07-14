3cda942bf4c92a7f0d12b804f808e69d6896795d
Zeph Lafassett <zeph@lafassett.com>
2014-07-14 10:30:30 -0700

    changing img static dir to use upload path from config file

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var router = require('./routes/index')(Router);
 app.use('/', router);
 
 app.use('/', serveStatic(path.join(__dirname, '/views/static')));
-app.use('/img', serveStatic(path.join(__dirname, '/uploads')));
+app.use('/img', serveStatic(path.join(__dirname, config.uploadPath)));
 
 app.listen(3000, function () {
 	logger.info("listening on port: 3000");
